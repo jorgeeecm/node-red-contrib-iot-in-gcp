5d44b59503b74e70eee639528ad7b4c457e207ba
elesus <39882319+elesus@users.noreply.github.com>
2022-12-09 18:40:02 +0100

    Added missing Project ID in the pub/sub send
    Added missing projectId in the pub/sub send section.

--- pubsub.js
+++ pubsub.js
@@ -142,6 +142,10 @@ module.exports = function(RED) {
         if (config.account) {
             credentials = GetCredentials(config.account);
         }
+	let projectId = config.projectId;
+        if (!projectId || projectId.trim().length == 0) {
+            projectId = null;
+        }
         const keyFilename = config.keyFilename;
 
         if (!config.topic) {
@@ -183,14 +187,18 @@ module.exports = function(RED) {
         // is an error.  If both are supplied, then credentials will be used.
         if (credentials) {
             pubsub = new PubSub({
+                "projectId": projectId,
                 "credentials": credentials
             });
         } else if (keyFilename) {
             pubsub = new PubSub({
+                "projectId": projectId,
                 "keyFilename": keyFilename
             });
         } else {
-            pubsub = new PubSub({});
+            pubsub = new PubSub({
+                "projectId": projectId
+			});
         }
 
         node.status(STATUS_CONNECTING);
